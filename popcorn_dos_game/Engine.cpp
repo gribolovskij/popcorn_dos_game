f7cbff59a788b58fd71d78682b12d2904c1a29b5
gribolovskij <romantsov1598@gmail.com>
2025-05-29 22:21:11 +0300

    Platform good

--- popcorn_dos_game/Engine.cpp
+++ popcorn_dos_game/Engine.cpp
@@ -7,7 +7,7 @@ enum Ebrick_Type
 	EBT_Blue,
 };
 
-HPEN Purple_Brick_Pen, Blue_Brick_Pen, Platform_Circle_Pen, Platform_Inner_Pen;
+HPEN Purple_Brick_Pen, Blue_Brick_Pen, Platform_Circle_Pen, Platform_Inner_Pen, Arc_Pen;
 HBRUSH Purple_Brick_Brush, Blue_Brick_Brush, Platform_Circle_Brush, Platform_Inner_Brush;
 
 const int Global_Scale = 1;
@@ -39,20 +39,21 @@ char Level_01[14][12] =
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 };
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+void Create_Pen_Brush(unsigned  char r, unsigned  char g, unsigned  char b, HPEN &pen, HBRUSH &brush)
+{// Функция реализации цвета RGB одной строкой
+	pen = CreatePen(PS_SOLID, 0, RGB(r, g, b));
+	brush = CreateSolidBrush(RGB(r, g, b));
+}
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 void Init()
 //	Функция инициализации |=|=|=| Настройка игры при старте
 {
-	Blue_Brick_Pen = CreatePen(PS_SOLID, 0, RGB(112, 146, 190));								 // Создаем цвет для 1-ого кирпича
-	Blue_Brick_Brush = CreateSolidBrush(RGB(112, 146, 190));
+	Create_Pen_Brush(112, 146, 190, Blue_Brick_Pen, Blue_Brick_Brush);
+	Create_Pen_Brush(255, 182, 89, Purple_Brick_Pen, Purple_Brick_Brush);
+	Create_Pen_Brush(155, 0, 0, Platform_Circle_Pen, Platform_Circle_Brush);
+	Create_Pen_Brush(249, 100, 0, Platform_Inner_Pen, Platform_Inner_Brush);
 
-	Purple_Brick_Pen = CreatePen(PS_SOLID, 0, RGB(255, 182, 89));								 // Создаем цвет для 2-ого кирпича
-	Purple_Brick_Brush = CreateSolidBrush(RGB(255, 182, 89));
-
-	Platform_Circle_Pen = CreatePen(PS_SOLID, 0, RGB(155, 0, 0));
-	Platform_Circle_Brush = CreateSolidBrush(RGB(155, 0, 0));
-
-	Platform_Inner_Pen = CreatePen(PS_SOLID, 0, RGB(249, 100, 0));
-	Platform_Inner_Brush = CreateSolidBrush(RGB(249, 100, 0));
+	Arc_Pen = CreatePen(PS_SOLID, 0, RGB(255, 255, 255));
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 void Draw_Brick(HDC hdc, int x, int y, Ebrick_Type brick_type)
@@ -99,19 +100,21 @@ void Draw_Level(HDC hdc)
 void Draw_Platform(HDC hdc, int x, int y)
 //	Рисуем платформу
 {
-	// Красим шарики платформы
-
+	// 1. Рисуем боковые шарики
 	SelectObject(hdc, Platform_Circle_Pen);
 	SelectObject(hdc, Platform_Circle_Brush);
 	Ellipse(hdc, x, y, x + Circle_Size* Global_Scale, y + Circle_Size * Global_Scale);
 	Ellipse(hdc, x + (Circle_Size+Inner_Width), y, x + ((Circle_Size*2)+Inner_Width), y+ Circle_Size);
 
-	// Красим середину платформы
-
+	// 2. Рисуем среднюю часть
 	SelectObject(hdc, Platform_Inner_Pen);
 	SelectObject(hdc, Platform_Inner_Brush);
 	RoundRect(hdc, x+9, y+18, x + (Inner_Width + 31), y+Global_Scale*2, 10 * Global_Scale, 32 * Global_Scale);
 						//x+ Inner_Width Расстояние между шариками, может увеличиваться!!!
+
+	// 3. Рисуем среднюю часть
+	SelectObject(hdc, Arc_Pen);
+	Arc	(hdc, 363, 563, 353, 552, 360, 489, 179, 489 );
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 void Draw_Frame(HDC hdc)
@@ -120,6 +123,5 @@ void Draw_Frame(HDC hdc)
 	Draw_Level(hdc);
 
 	Draw_Platform(hdc, 350, 550);
-
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
