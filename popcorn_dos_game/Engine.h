ba166aac267e6502562fcf3a016d51be780f0299
gribolovskij <romantsov1598@gmail.com>
2025-06-25 15:22:04 +0300

    Private

--- popcorn_dos_game/Engine.h
+++ popcorn_dos_game/Engine.h
@@ -30,23 +30,29 @@ const int Timer_ID = WM_USER + 1;
 class ALevel
 {
 public:
-	void Init_Level();
 	void Check_Level_Hit_Brick(int &next_y_pos, double &ball_direction);
-	void Draw_Brick(HDC hdc, int x, int y, Ebrick_Type brick_type);
-	void Set_Brick_Letter_Colors(bool is_switch_color, HPEN& front_pen, HBRUSH& front_brush, HPEN& back_pen, HBRUSH& back_brush);
-	void Draw_Brick_Letter(HDC hdc, int x, int y, Ebrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
+	void Init_Level();
 	void Draw_Level(HDC hdc);
 
-	HPEN Purple_Brick_Pen, Blue_Brick_Pen, Letter_Pen;
-	HBRUSH Purple_Brick_Brush, Blue_Brick_Brush;
-	RECT Level_Rect, paint_area;
-
-	static const int Level_Width = 12;		// Width brick
 	static const int Level_Height = 14;		// Height brick
+	static const int Level_Width = 12;		// Width brick
 	static const int Level_X_Offset = 23;		// Position start level
 	static const int Level_Y_Offset = 13;		// Position start level
 	static const int Cell_Width = 64;
 	static const int Cell_Height = 26;
+
+	RECT Level_Rect;
+
+private:
+
+	void Draw_Brick(HDC hdc, int x, int y, Ebrick_Type brick_type);
+	void Set_Brick_Letter_Colors(bool is_switch_color, HPEN& front_pen, HBRUSH& front_brush, HPEN& back_pen, HBRUSH& back_brush);
+	void Draw_Brick_Letter(HDC hdc, int x, int y, Ebrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
+
+	HPEN Purple_Brick_Pen, Blue_Brick_Pen, Letter_Pen;
+	HBRUSH Purple_Brick_Brush, Blue_Brick_Brush;
+	RECT paint_area;
+
 	static const int Brick_Width = 61;
 	static const int Brick_Height = 23;
 
